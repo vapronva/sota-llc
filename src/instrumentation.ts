664403b7cfea474e1c9d8f2a43f4a9e90d1c3ceb
Vladimir (vapronva) <git@vprw.ru>
2026-02-20 14:37:32 +0300

    fix[sentry]: decouple edge config import

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,12 +3,9 @@ import * as Sentry from "@sentry/nextjs";
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("../sentry.server.config");
-  } else if (process.env.NEXT_RUNTIME === "edge") {
+  }
+  if (process.env.NEXT_RUNTIME === "edge") {
     await import("../sentry.edge.config");
-  } else {
-    throw new Error(
-      `[Sentry] Unsupported or undefined NEXT_RUNTIME: "${process.env.NEXT_RUNTIME}". Expected "nodejs" or "edge". Sentry will not be initialized.`,
-    );
   }
 }
 
